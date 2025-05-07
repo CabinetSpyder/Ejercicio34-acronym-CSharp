e17b37e0739b9694f45afde8ed71e97be23729cb
¨CabinetSpyder¨ <andres.artaloytia@gmail.com>
2025-05-07 11:28:33 +0200

    Primera iteracion de la solucion

--- acronym/Acronym.cs
+++ acronym/Acronym.cs
@@ -1,7 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+
+        if(string.IsNullOrWhiteSpace(phrase))
+        {
+            return "";
+        }
+
+        var auxStr = new StringBuilder();
+        bool pickNextLetter = false;
+        
+        auxStr.Append(phrase[0]);
+
+        foreach(char ch in phrase)
+        {
+            
+            if(pickNextLetter && char.IsLetter(ch))
+            {
+                auxStr.Append(ch);
+                pickNextLetter = false;
+            }
+
+            if(char.IsWhiteSpace(ch) || ch == '-')
+            {
+                pickNextLetter = true;
+            }
+            
+        }
+
+        return auxStr.ToString().ToUpper();
     }
 }
